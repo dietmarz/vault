0a3024fed7698a730c335d6396273545638d758a
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-03-12 14:51:42 +0100

    Update vault by sv

--- kh/util/bin/gitOverwriteHistory.sh
+++ kh/util/bin/gitOverwriteHistory.sh
@@ -2,18 +2,23 @@
 
 set -eu
 
-# Der Pfad zur Datei, die überschrieben werden soll.
-FILE=$1
+# FILE_PATH="$1"
+#
+# cp "$FILE_PATH" "/tmp/file_to_replace_in_git_history"
+#
+# FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --tree-filter "
+#     cp '/tmp/file_to_replace_in_git_history' '$FILE_PATH'
+# " --tag-name-filter cat -- --all
+#
+# rm "/tmp/file_to_replace_in_git_history"
 
-# Die aktuelle Version der Datei sichern.
-cp $FILE /tmp/temp_file_backup
 
-# Die gesamte Historie der Datei durch die aktuelle Version ersetzen.
-git filter-branch --force --index-filter \
-"git rm --cached --ignore-unmatch $FILE; \
-cp /tmp/temp_file_backup $FILE; \
-git add $FILE" --prune-empty --tag-name-filter cat -- --all
 
-# Die temporäre Datei entfernen.
-rm /tmp/temp_file_backup
+# https://stackoverflow.com/questions/78140146/git-replace-file-content-in-history
+# https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#EXAMPLES
+SRC_FILE="$1"
+TO_REPLACE_PATH="$1"
 
+git filter-repo --force --commit-callback "for change in commit.file_changes:
+    if (change.type != b'D' and change.filename == b'${TO_REPLACE_PATH}'):
+        change.blob_id = b'$(git hash-object -w ${SRC_FILE})'"
