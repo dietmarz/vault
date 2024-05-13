5f619443dcca6843cd0a180cbd9a3cf3bd9475f9
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-05-13 13:09:14 +0200

    Update vault by sv

--- kh/util/bin/lastModifiedNotes.sh
+++ kh/util/bin/lastModifiedNotes.sh
@@ -7,7 +7,7 @@ if [ -z "${ENV_VAULT}" ]; then
     exit 1
 fi
 
-LINES=20
+LINES=40
 
 echo "Show last ${LINES} modified files of ${ENV_VAULT}"
 
@@ -15,7 +15,8 @@ echo "Show last ${LINES} modified files of ${ENV_VAULT}"
 pushd "${ENV_VAULT}"
 
 # Auflisten der zuletzt geänderten Dateien, ohne die im .daily Verzeichnis
-git ls-files -z | grep -vzZ '.daily' | xargs -0 -n1 -I{} git log -1 --date=iso --format="%ad {}" {} | sed 's/\+.* //' | sort | tail -n $LINES
+git ls-files -z | grep -vzZ '.daily' | xargs -0 -n1 -I {} git log -1 --date=iso --format="%ad {}" -- {} | sed 's/\+.* //' | sort | tail -n $LINES
+
 
 # Zurück zum ursprünglichen Verzeichnis
 popd
