997ff4daaa8fbd853895115b964bcd194f848fb7
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-05-18 10:20:04 +0200

    Update vault by sv

--- kh/util/bin/format-adoc.sh
+++ kh/util/bin/format-adoc.sh
@@ -6,13 +6,13 @@ if [[ -z "${ENV_VAULT}" ]]; then
     exit 1
 fi
 
-# Gehe durch alle .adoc-Dateien im ENV_VAULT Verzeichnis
-for file in "${ENV_VAULT}"/*.adoc; do
-    if [[ -f $file ]]; then
+# Gehe rekursiv durch alle .adoc-Dateien im ENV_VAULT Verzeichnis
+find "${ENV_VAULT}" -type f -name "*.adoc" | while read -r file; do
+  if [[ -f $file ]]; then
         # Ersetze '** ' am Zeilenanfang durch ' **'
         sed -i -E 's/^\*\* /  ** /' "$file"
-        echo "Bearbeitet: $file"
+        # echo "Bearbeitet: $file"
     fi
 done
 
-echo "Alle Dateien wurden bearbeitet."
+echo "Alle adoc dateien wurden formatiert"
